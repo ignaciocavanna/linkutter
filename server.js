75776155c6238204dc20bbe8326e064b9f418e8b
ignaciocavanna <148281997+ignaciocavanna@users.noreply.github.com>
2025-01-12 20:52:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.get("/", async (req, res) => {
     short_link: "",
     long_link: "",
     links: links,
-    isAuthenticated: true,
+    isAuthenticated: isAuthenticated(),
     user: req.user,
   });
 });
